c6f9aac512cb36e22c12384425bbdb6dc3af6fa5
xushengan <740770819@qq.com>
2018-04-23 20:59:53 +0800

    day 4

--- boards/views.py
+++ boards/views.py
@@ -25,3 +25,8 @@ def home(request):
 def board_topics(request, pk):
     board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html',{'board': board})
+
+
+def new_topic(request,pk):
+    board =  get_object_or_404(Board, pk=pk)
+    return render(request, 'new_topic.html',{'board':board})
--- myproject/urls.py
+++ myproject/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns(
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', 'boards.views.home', name='home'),
     url(r'^boards/(?P<pk>\d+)/$','boards.views.board_topics',name='board_topics'),
+    url(r'^boards/(?P<pk>\d+)/new/$','boards.views.new_topic',name='new_topic'),
     url(r'^admin/', include(admin.site.urls)),
 )
